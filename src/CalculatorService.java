55ea0748d0744fa52280acfcb6fcc069d2de34d1
Jake Schultz <iamjake648@icloud.com>
2016-08-09 10:57:13 -0500

    Adds Java -> Javascript bridge for future interface overhaul
    Adds proof of concept for a future interface overhaul done with
    HTML5/CSS/JS.
    
    Idea and bridge code came from: https://github.com/lipido/javafxwebview
    
    Initial commit does not contain any HTML5/JS assets yet.

---
+++ src/CalculatorService.java
@@ -0,0 +1,7 @@
+public class CalculatorService {
+
+	// sync function
+	public int sum(int a, int b){
+		return a + b;
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -22,81 +22,88 @@ public class Main {
     private static Preferences prefs = Preferences.userNodeForPackage(UserPrompt.class);
 
     public static void main(String[] args) throws java.lang.Exception{
-        //Start the util normally
-        if (args.length == 0){
-            //If it is a linux server, don't prompt, and use the text interface
-            if (System.getProperty("os.name").contains("Linux")){
-                new UserPromptHeadless();
-                //Present the GUI
-            } else {
-                ConfigurationController con = new ConfigurationController();
-                //Loop until you can get a valid config xml path.
-                    while(!con.canGetFile()){
-                        if (!(con.attemptAutoDiscover())){
+        Boolean HTMLInterface = false;
+        if (HTMLInterface){
+            HealthReport2.launchHealthReport2(args);
+        } else {
+
+            //Start the util normally
+            if (args.length == 0) {
+                //If it is a linux server, don't prompt, and use the text interface
+                if (System.getProperty("os.name").contains("Linux")) {
+                    new UserPromptHeadless();
+                    //Present the GUI
+                } else {
+                    ConfigurationController con = new ConfigurationController();
+                    //Loop until you can get a valid config xml path.
+                    while (!con.canGetFile()) {
+                        if (!(con.attemptAutoDiscover())) {
                             JFileChooser chooser = new JFileChooser();
                             chooser.setDialogTitle("Select Configuration XML File");
                             FileNameExtensionFilter filter = new FileNameExtensionFilter(
                                     "XML Files", "xml");
                             chooser.setFileFilter(filter);
                             int returnVal = chooser.showOpenDialog(null);
-                            if(returnVal == JFileChooser.APPROVE_OPTION) {
+                            if (returnVal == JFileChooser.APPROVE_OPTION) {
                                 //Update the path
                                 prefs.put("config_xml_path", chooser.getSelectedFile().getAbsolutePath());
                             } else {
                                 System.exit(0);
                             }
                         }
-                } try {
-                    //Open the GUI for Windows and Mac
-                    new UserPrompt();
-                } catch (java.lang.Exception e){
-                    //Catch any errors encountered, display them in a popup and print them.
-                    System.out.print(e);
-                    JOptionPane.showMessageDialog(new JFrame(),"A fatal error has occurred.\n"+e, "Fatal Error", JOptionPane.ERROR_MESSAGE);
-                    e.printStackTrace();
+                    }
+                    try {
+                        //Open the GUI for Windows and Mac
+                        new UserPrompt();
+                    } catch (java.lang.Exception e) {
+                        //Catch any errors encountered, display them in a popup and print them.
+                        System.out.print(e);
+                        JOptionPane.showMessageDialog(new JFrame(), "A fatal error has occurred.\n" + e, "Fatal Error", JOptionPane.ERROR_MESSAGE);
+                        e.printStackTrace();
+                    }
+
+
                 }
-
-
-            }
-        //They provided the args to start the headless version OR the monitor
-        } else {
-            if (args[0].equals("-h")){
-              new UserPromptHeadless();
-            }
-            if (args[0].equals("-m")){
-                Scanner scanner = new Scanner(System.in);
-                ConfigurationController con = new ConfigurationController();
-                while(!con.canGetFile()){
-                    if (!(con.attemptAutoDiscover())){
-                        System.out.println("Path to Config.xml not found. Please type the full path below or type 'exit' to close the program. ");
-                        String path = scanner.next();
-                        //If they type exit to grab the XML, exit.
-                        if (path.equals("exit")){
-                            System.exit(0);
-                        } else {
-                            prefs.put("config_xml_path", path);
+                //They provided the args to start the headless version OR the monitor
+            } else {
+                if (args[0].equals("-h")) {
+                    new UserPromptHeadless();
+                }
+                if (args[0].equals("-m")) {
+                    Scanner scanner = new Scanner(System.in);
+                    ConfigurationController con = new ConfigurationController();
+                    while (!con.canGetFile()) {
+                        if (!(con.attemptAutoDiscover())) {
+                            System.out.println("Path to Config.xml not found. Please type the full path below or type 'exit' to close the program. ");
+                            String path = scanner.next();
+                            //If they type exit to grab the XML, exit.
+                            if (path.equals("exit")) {
+                                System.exit(0);
+                            } else {
+                                prefs.put("config_xml_path", path);
+                            }
                         }
                     }
-                }
 
-                String xml_path = prefs.get("config_xml_path","Path to file '/Users/user/desktop/config.xml'");
-                SAXBuilder builder = new SAXBuilder();
-                File xmlFile = new File(xml_path);
-                try {
-                    Document document = (Document) builder.build(xmlFile);
-                    Element root = document.getRootElement();
-                    HealthCheck hc = new HealthCheck();
-                    int mobile_device_length = hc.checkAPILength(root.getChild("jss_url").getValue(), root.getChild("jss_username").getValue(), root.getChild("jss_password").getValue(), "mobiledevicecommands");
-                    int computer_length = hc.checkAPILength(root.getChild("jss_url").getValue(), root.getChild("jss_username").getValue(), root.getChild("jss_password").getValue(), "computercommands");
-                    writeLogEntry(mobile_device_length, computer_length);
-                } catch (java.lang.Exception e) {
-                    System.out.print(e);
-                    System.out.println("Config XML file damaged. Unable to run monitor.");
-                }
+                    String xml_path = prefs.get("config_xml_path", "Path to file '/Users/user/desktop/config.xml'");
+                    SAXBuilder builder = new SAXBuilder();
+                    File xmlFile = new File(xml_path);
+                    try {
+                        Document document = (Document) builder.build(xmlFile);
+                        Element root = document.getRootElement();
+                        HealthCheck hc = new HealthCheck();
+                        int mobile_device_length = hc.checkAPILength(root.getChild("jss_url").getValue(), root.getChild("jss_username").getValue(), root.getChild("jss_password").getValue(), "mobiledevicecommands");
+                        int computer_length = hc.checkAPILength(root.getChild("jss_url").getValue(), root.getChild("jss_username").getValue(), root.getChild("jss_password").getValue(), "computercommands");
+                        writeLogEntry(mobile_device_length, computer_length);
+                    } catch (java.lang.Exception e) {
+                        System.out.print(e);
+                        System.out.println("Config XML file damaged. Unable to run monitor.");
+                    }
 
-            }
-            if (args[0].equals("-g")){
-                new MonitorGraph("");
+                }
+                if (args[0].equals("-g")) {
+                    new MonitorGraph("");
+                }
             }
         }
 
